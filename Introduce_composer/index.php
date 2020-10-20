9899538eacadbc820009dbd5f57508058fe7bb3d
rezaandika142 <70564413+rezaandika142@users.noreply.github.com>
2020-10-20 10:42:45 +0700

    Finish

---
+++ Introduce_composer/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'vendor/autoload.php';
+
+$mail=new PHPMailer();
+
+var_dump($mail);
---
+++ phpmailer/index.php
@@ -0,0 +1,137 @@
+<?php
+
+$pengirim = $_POST['awal'].' '.$_POST['akhir'];
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\OAuth;
+// Alias the League Google OAuth2 provider class
+use League\OAuth2\Client\Provider\Google;
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require "vendor/autoload.php";
+require "config_api.php";
+
+$mail =new PHPMailer();
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// SMTP::DEBUG_OFF = off (for production use)
+// SMTP::DEBUG_CLIENT = client messages
+// SMTP::DEBUG_SERVER = client and server messages
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption mechanism to use - STARTTLS or SMTPS
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Set AuthType to use XOAUTH2
+$mail->AuthType = 'XOAUTH2';
+
+//Fill in authentication details here
+//Either the gmail account owner, or the pengirim that gave consent
+$email = 'rezaandika1234509876@gmail.com';
+$clientId = '727917382685-9foclnuh4e4fkcndo7a4pu6ugk54kfsv.apps.googleusercontent.com';
+$clientSecret = '-TGXaEzC3mEghYhdVTxi8I_X';
+
+//Obtained by configuring and running get_oauth_token.php
+//after setting up an app in Google Developer Console.
+$refreshToken = ' 1//0gjHxFw5pyM08CgYIARAAGBASNwF-L9IrJg5ZYZtUKGq2QdQuPtWaffghg4ijGpat2u_zDCJxO-EjwQoCd7teci5xCEivmiFI8cs';
+
+//Create a new OAuth2 provider instance
+$provider = new Google(
+    [
+        'clientId' => $clientId,
+        'clientSecret' => $clientSecret,
+    ]
+);
+
+//Pass the OAuth provider instance to PHPMailer
+$mail->setOAuth(
+    new OAuth(
+        [
+            'provider' => $provider,
+            'clientId' => $clientId,
+            'clientSecret' => $clientSecret,
+            'refreshToken' => $refreshToken,
+            'userName' => $email,
+        ]
+    )
+);
+
+//Set who the message is to be sent from
+//For gmail, this generally needs to be the same as the pengirim you logged in as
+$mail->setFrom($email, $pengirim);
+
+//Set who the message is to be sent to
+$mail->addAddress(($_POST['email']), 'kamu');
+
+//Set the subject line
+$mail->Subject = 'Reza Andika Pratama';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->CharSet = PHPMailer::CHARSET_UTF8;
+$mail->msgHTML(($_POST['critic']), __DIR__);
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+// $mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+// if (!$mail->send()) {
+//     echo 'Mailer Error: ' . $mail->ErrorInfo;
+// } else {
+//     echo 'Message sent!';
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="col-5">
+                <?php if (!$mail->send()) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo "Error Data Tidak Boleh Kosong!!!" ?>
+                    </div>
+                <?php else : ?>
+                    <div class="alert alert-primary" role="alert">
+                        <?php echo "Data Terkirim" ?>
+                    </div>
+                <?php endif ?>
+            </div>
+            <button class="btn btn-outline-secondary"><a href="send.html">Kirim ulang</a></button>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
